a10671dc6ebd579bc64a09b5eca669f661d9eb91
Oisín <oinolan@tcd.ie>
2019-02-28 19:03:59 +0000

    Graph api added

---
+++ HashCode2019/src/Graph.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Graph {
+	ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+}
---
+++ HashCode2019/src/Vertex.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Vertex {
+	public ArrayList<String> tags = new ArrayList<String>();
+	public ArrayList<Edge> edges = new ArrayList<Edge>();
+	public int id;
+}
